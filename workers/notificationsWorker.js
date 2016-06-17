ed82aff781aedf339862cde35e1e273c4b3dff1f
Kayla Thomas <kaymthom@yahoo.com>
2016-06-17 10:51:32 -0500

    fixed tbe error where text schema wasn't being called

--- workers/notificationsWorker.js
+++ workers/notificationsWorker.js
@@ -1,9 +1,11 @@
 var Appointment = require('../models/appointment');
+var Text = require('../models/text');
 
 var notificationWorkerFactory =  function(){
   return {
     run: function(){
       Appointment.sendNotifications();
+      Text.sendNotifications();
     }
   };
 };
